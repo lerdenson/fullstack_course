d2cf68faf0feb05670176db1fb4b4843ec55f1e2
Maxim Morozov <74878962+lerdenson@users.noreply.github.com>
2022-12-13 12:15:56 +0300

    Update App.js

--- task1/part2/courseinfo/src/App.js
+++ task1/part2/courseinfo/src/App.js
@@ -46,7 +46,7 @@ const App = () => {
     ]
 
     return <div>
-        {courses.map(course => <Course course={course}/>)}
+        {courses.map(course => <Course key={course.id} course={course}/>)}
     </div>
 }
 
